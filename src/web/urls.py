daea27cc7538d9f9317389e9313a5acbc5faad99
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2009-08-07 18:56:13 +0200

    Basic web interface now working. Need to implement deletion of records, setting reason, outcome, etc

--- src/web/urls.py
+++ src/web/urls.py
@@ -1,12 +1,23 @@
 from django.conf.urls.defaults import *
+from django.views.generic import list_detail
+from django.conf import settings
+
+from sumatra.recordstore.django_store.models import SimulationRecord
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+simulation_records = {
+    "queryset": SimulationRecord.objects.all(),
+    "template_name": "simulation_list.html",
+}
+
 urlpatterns = patterns('',
     # Example:
-    (r'^$', 'sumatra.web.views.index'),
+    (r'^$', list_detail.object_list, simulation_records),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    (r'^(?P<id>\w+[\w\-\.]*)/$', 'sumatra.web.views.simulation_detail'),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
--- src/web/views.py
+++ src/web/views.py
@@ -1,4 +1,35 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from sumatra.recordstore.django_store import models
 
 def index(request):
     return HttpResponse("Welcome to the Sumatra web interface.")
+
+def simulation_detail(request, id):
+    record = models.SimulationRecord.objects.get(id=id)
+    #SimulationUpdateForm = forms.form_for_instance(record, fields=('reason', 'outcome', 'tags'))
+    
+    #if request.method == 'POST':
+    #    if request.POST.has_key('delete'):
+    #        record.delete()
+    #        return HttpResponseRedirect('/')
+    #    else:
+    #        form = SimulationUpdateForm(request.POST)
+    #        if form.is_valid():
+    #            form.save()
+    #        return HttpResponseRedirect('/')
+    #else:
+    #    form = SimulationUpdateForm()
+    
+    #try:
+    #    data_archive = tarfile.open(os.path.join(DATAROOT, record.id + ".tar.gz"),'r:gz')
+    #    archive_contents = data_archive.getmembers()
+    #    data_archive.close()
+    #except IOError:
+    #    archive_contents = []
+    
+    return render_to_response('simulation_detail.html', {'record': record,
+                                                         #'parameters': record.iterparams(),
+                                                         #'datafiles': archive_contents,
+                                                         #'form': form
+                                                         })
