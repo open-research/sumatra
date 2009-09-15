8b7aabf60a2a6ef3e1148436be7b61b7108ec40a
Unknown <andrew@NeuroinformatiqueUNIC.local>
2009-09-15 11:35:46 +0200

    Added setup.py and other files needed for distribution

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+      
+setup(
+    name = "Sumatra",
+    version = "0.1",
+    package_dir={'sumatra': 'src'},
+    packages = ['sumatra',],
+    scripts= ['src/smt',],
+    author = "Andrew P. Davison",
+    author_email = "andrewpdavison@gmail.com",
+    description = "A tool for managing simulation-based projects",
+    long_description = """A tool for managing simulation-based projects.""",
+    license = "CeCILL http://www.cecill.info",
+    keywords = "computational neuroscience simulation project-management",
+    url = "http://neuralensemble.org/Sumatra/",
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'Environment :: Console',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Science/Research',
+                   'License :: Other/Proprietary License',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering'],
+)
+
