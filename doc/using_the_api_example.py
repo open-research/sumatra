798b7ed18c9c298d9db5326d3917f0294b965bb9
Unknown <andrew@dhcp-12-56.ens.fr>
2010-07-09 13:09:03 +0200

    Updated the "using the API" docs and examples

--- doc/using_the_api_example.py
+++ doc/using_the_api_example.py
@@ -4,23 +4,25 @@ import time
 from sumatra.projects import load_project
 from sumatra.parameters import build_parameters
 
-project = load_project()
-start_time = time.time()
-    
+def main(parameters):
+    numpy.random.seed(parameters["seed"])
+    distr = getattr(numpy.random, parameters["distr"])
+    data = distr(size=parameters["n"])    
+    output_file = "%s.dat" % parameters["sumatra_label"]
+    numpy.savetxt(output_file, data)
+
 parameter_file = sys.argv[1]
 parameters = build_parameters(parameter_file)
-    
+
+project = load_project()    
 record = project.new_record(parameters=parameters,
                             main_file=__file__,
-                            label="api_example",
                             reason="reason for running this simulation")
+parameters.update({"sumatra_label": record.label})
+start_time = time.time()
 
-numpy.random.seed(parameters["seed"])
-distr = getattr(numpy.random, parameters["distr"])
-data = distr(size=parameters["n"])
-    
-numpy.savetxt("%s.dat" % record.label, data)
-    
+main(parameters)
+
 record.duration = time.time() - start_time
 record.data_key = record.datastore.find_new_files(record.timestamp)
 project.add_record(record)
--- doc/using_the_api_example2.py
+++ doc/using_the_api_example2.py
@@ -2,9 +2,6 @@ import numpy
 import sys
 from sumatra.parameters import build_parameters
 from sumatra.decorators import capture
-    
-parameter_file = sys.argv[1]
-parameters = build_parameters(parameter_file)
 
 @capture
 def main(parameters):
@@ -12,5 +9,7 @@ def main(parameters):
     distr = getattr(numpy.random, parameters["distr"])
     data = distr(size=parameters["n"])
     numpy.savetxt("%s.dat" % parameters["sumatra_label"], data)
-  
+
+parameter_file = sys.argv[1]
+parameters = build_parameters(parameter_file)
 main(parameters)
