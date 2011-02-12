e73edf68ac9641db5ae44a489afb39522b96dc44
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2011-02-12 19:52:47 +0100

    Tiny test fix.

--- test/unittests/test_programs.py
+++ test/unittests/test_programs.py
@@ -39,8 +39,8 @@ class TestExecutable(unittest.TestCase):
         self.assertEqual(prog.path, "/bin/ls")
         
     def test__init__should_find_version_if_possible(self):
-        prog = Executable("/bin/ls")
-        self.assertEqual(prog.version, None) # this is true on Darwin, but not on Ubuntu
+        #prog = Executable("/bin/ls")
+        #self.assertEqual(prog.version, None) # this is true on Darwin, but not on Ubuntu
         prog = Executable(sys.executable)
         python_version = "%d.%d.%d" % tuple(sys.version_info[:3])
         self.assertEqual(prog.version, python_version)
