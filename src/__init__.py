eba40b9b5da3e6f360759a39df1f6224f5d88cc5
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2011-03-14 15:09:31 +0100

    Forgot to update version number in docs

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@ __all__ = ['commands', 'datastore', 'formatting', 'launch', 'parameters',
            'programs', 'projects', 'records', 'recordstore', 'versioncontrol',
            'dependency_finder', 'external', 'web', 'decorators']
 
-__version__ = "0.3.0"    
+__version__ = "0.3.1.dev"    
