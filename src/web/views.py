1eac63ec120fc091bbf286a2b83fc003a1248cde
Unknown <Samarkanov@Samarkanov-PC.vinci.ec-lille.fr>
2012-06-07 21:52:22 +0200

    record_list.html: Search of the label. Correction of several bugs detected during the rendering of results

--- src/web/views.py
+++ src/web/views.py
@@ -348,12 +348,17 @@ def settings(request, project):
 def search(request, project, item):
     ''' search dropdown list from record_list.html ''' 
     web_settings = load_project().web_settings      
-    nb_per_page = int(load_project().web_settings['nb_records_per_page'])
+    nb_per_page = int(web_settings['nb_records_per_page'])
     results = models.Record.objects.filter(label__icontains=item)
     return list_detail.object_list(request,
                               queryset=results,
                               template_name="record_list.html",
-                              paginate_by=nb_per_page)
+                              paginate_by=nb_per_page,
+                              extra_context={'project_name': project,
+                                             'settings':web_settings,
+                                             'records_per_page': nb_per_page,
+                                             'search_info':{'label':item}
+                                             })
 def short_repo(url_repo):
     return '%s\%s' %(url_repo.split('\\')[-2], 
                      url_repo.split('\\')[-1])
