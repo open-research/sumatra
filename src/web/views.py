b2ea3fd52b38a65550164c9067e816126e8143e3
Unknown <samarkanov@ubuntu>
2012-08-02 12:18:09 +0400

    New function initSettings which initializes the web settings for the projects that don't have this settings

--- src/web/views.py
+++ src/web/views.py
@@ -29,25 +29,38 @@ class RecordForm(SearchForm):
         fields = ('label', 'tags', 'reason', 'executable', 'repository',
                   'main_file', 'script_arguments', 'timestamp')
 
+def initSettings(project, option='web'):
+    '''Checking existence of the specific settings in .smt/project.
+    In case it doesn't exist, it will be initialized with some default values
+    Inputs:
+        project: project object,
+        option: string.
+    Output:
+        web_settings: dictionary.
+    '''
+    if option == 'web':
+        try:
+            return project.web_settings
+        except AttributeError:
+            project.web_settings = init_websettings()   
+            for key, item in project.web_settings.iteritems():
+                if item:
+                    project.web_settings[key] = item
+            project.save()
+            return project.web_settings
+    else:
+        pass
+
+
 def list_records(request, project):
-    nbCols = 14 
-    form = RecordForm()  
-    project_loaded = load_project()
-    sim_list = models.Record.objects.filter(project__id=project).order_by('-timestamp')
-    try:
-        web_settings = load_project().web_settings
-    except AttributeError:
-        project_loaded.web_settings = init_websettings()   
-    for key, item in project_loaded.web_settings.iteritems():
-        if item:
-            project_loaded.web_settings[key] = item
-    project_loaded.save()
-    web_settings = load_project().web_settings
+    form = RecordForm() # search form  
+    loaded_prj = load_project() # project object
+    sim_list = models.Record.objects.filter(project__id=project).order_by('-timestamp') # project: string
+    web_settings = initSettings(loaded_prj)
     nb_per_page = int(web_settings['nb_records_per_page'])
     paginator = Paginator(sim_list, nb_per_page)
-    # get names of all files in the current directory:
-    files = os.listdir(os.getcwd())
-    page_list = paginator.page(1)
+    files = os.listdir(os.getcwd()) # names of all files and folders in current directory
+    page_list = paginator.page(1) # always open the first page
     if web_settings['table_HideColumns'] is not None:
         nbCols_actual = NbCol - len(web_settings['table_HideColumns'])
     else:
@@ -66,5 +79,5 @@ def list_records(request, project):
            'width':{'head': head_width, 'label':label_width},
            'active':'List of records',
            'files': files,
-           'path':project_loaded.default_executable.path}
+           'path':loaded_prj.default_executable.path}
     return render_to_response('record_list.html', dic)
