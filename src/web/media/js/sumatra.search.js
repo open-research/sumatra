95e471987ddcbe1f65c3c6fe47ffae2a8e8761f1
Unknown <samarkanov@ubuntu>
2012-08-11 14:06:11 +0400

    search: the searched items are repeated in the input textarea onthe header of the page

--- src/web/media/js/sumatra.search.js
+++ src/web/media/js/sumatra.search.js
@@ -2,12 +2,19 @@ function getSearchFormOb(){
     var $inputs = $('#search_form :input');
     var $fulltext_inquiry = $('#search_subnav').val(); //inquiry out of search form: using only the #search_subnav input
     var values = {};
+    var txt_inquiry = '';
     if ($fulltext_inquiry){ // ignore here the search form: only the content of #search_subnav
         values['fulltext_inquiry'] = $fulltext_inquiry;
     }else{
         $inputs.each(function() {
-            if (this.name) values[this.name] = $(this).val();
+        	var $val = $(this).val();
+            if (this.name) values[this.name] = $val;
+            // fill the #search_subnav. Example: user defined label = 777 in the search form. After it should print
+		    // 'label: 777' in the input textarea in the header of the page.  
+		    if ($val){  if (txt_inquiry) {txt_inquiry += ', ' + this.name + ': ' + $val;}
+		    			else {txt_inquiry += this.name + ': ' + $val; } }
         });
+        $('#search_subnav').val(txt_inquiry);
         values['date_interval'] = $('#sdate').html(); // date interval is out of form, so we add it explicitely:
         values['date_interval_from'] = $('#id_datewithin').val();
     }
@@ -33,6 +40,11 @@ $(function() {
     $('#icon_cdown').click(function(event){   
         var parentWidth = $('input.span5')[0].offsetWidth - 2; // the width of this drop-down equals the width of #search_subnav
         $('#menu_search').css('width', parentWidth).show();
+        // if it is clicked, we flush the content of the search input:
+        search_input_content = $('#search_subnav').val();
+        if (search_input_content.indexOf(':') < 0){ // if it doesn't contain the inquiry originally went from search form
+        	$('#search_subnav').val('');
+        };
     });
     // user can close the drop-down search list by clicking on the window or by clicking #close_menu (little icon top-right side)
     $('#close_menu, html').click(function() {
@@ -49,9 +61,6 @@ $(function() {
     $('#search_form').find(':input').each(function() {
          $(this).val('');
     });
-
-    // search input textarea
-    $('#search_subnav').val('');
 
     // as soon as user clicks the search button in the search drop-down list, the form will be submited
     $('#btn-search_form').click(function(){
