f2db409442082520a90b36d95be83741910c03fb
Unknown <samarkanov@ubuntu>
2012-08-15 19:37:59 +0400

    Search by name of parameter is finished. On the web search form we now have 'variable name' drop-down list with the list of all parameters.

--- src/recordstore/django_store/models.py
+++ src/recordstore/django_store/models.py
@@ -120,6 +120,9 @@ class Repository(BaseModel):
 class VariableSet(BaseModel):
     var_name = models.CharField(max_length=30)
 
+    def __unicode__(self):
+        return self.var_name
+
 
 class ParameterSet(BaseModel):
     type = models.CharField(max_length=30)
