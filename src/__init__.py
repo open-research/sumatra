c1f34a9ea7bb21328cd60153697eb1d43267fff7
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2013-04-02 15:18:18 +0200

    Updated version numbers for 0.5.2 release.

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,4 @@ __all__ = ['commands', 'datastore', 'formatting', 'launch', 'parameters',
            'dependency_finder', 'external', 'web', 'decorators', 'publishing',
            'users', 'core']
 
-__version__ = "0.5.1"    
+__version__ = "0.5.2"    
