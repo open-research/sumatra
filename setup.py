1e4e93ccb04e5bbfbb2235124f5869bbae40c4c2
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2015-06-19 17:25:54 +0200

    Removed claims that we support Python 3.3, since we don't test it with Travis, and it is unlikely anyone is using it who couldn't upgrade to 3.4.

--- setup.py
+++ setup.py
@@ -65,7 +65,6 @@ setup(
                    'Programming Language :: Python',
                    'Programming Language :: Python :: 2.6',
                    'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3.3',
                    'Programming Language :: Python :: 3.4',
                    'Topic :: Scientific/Engineering'],
     cmdclass = {'sdist': sdist_git},
