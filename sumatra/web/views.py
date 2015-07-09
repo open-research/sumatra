b4ef56c22e838b480e0debc8edb784e0c9407d7b
Sebastian Spreizer <spreizer@web.de>
2015-07-09 13:39:21 +0200

    website in read-only

--- sumatra/web/views.py
+++ sumatra/web/views.py
@@ -45,7 +45,13 @@ class ProjectDetailView(DetailView):
     def get_object(self):
         return Project.objects.get(pk=self.kwargs["project"])
 
+    def get_context_data(self, **kwargs):
+        context = super(ProjectDetailView, self).get_context_data(**kwargs)
+        context['read_only'] = settings.READ_ONLY
+        return context
+
     def post(self, request, *args, **kwargs):
+        if settings.READ_ONLY: return HttpResponse('It is in read-only mode.')
         name = request.POST.get('name', None)
         description = request.POST.get('description', None)
         project = self.get_object()
@@ -69,6 +75,7 @@ class RecordListView(ListView):
         context = super(RecordListView, self).get_context_data(**kwargs)
         context['project'] = Project.objects.get(pk=self.kwargs["project"])
         context['tags'] = Tag.objects.all()  # would be better to filter, to return only tags used in this project.
+        context['read_only'] = settings.READ_ONLY
         return context
 
 
@@ -91,9 +98,11 @@ class RecordDetailView(DetailView):
         if hasattr(parameter_set, "as_dict"):
             parameter_set = parameter_set.as_dict()
         context['parameters'] = parameter_set
+        context['read_only'] = settings.READ_ONLY
         return context
 
     def post(self, request, *args, **kwargs):
+        if settings.READ_ONLY: return HttpResponse('It is in read-only mode.')
         record = self.get_object()
         for attr in ("reason", "outcome", "tags"):
             value = request.POST.get(attr, None)
@@ -223,6 +232,7 @@ def parameter_list(request, project):
 
 
 def delete_records(request, project):
+    if settings.READ_ONLY: return HttpResponse('It is in read-only mode.')
     records_to_delete = request.POST.getlist('delete[]')
     delete_data = request.POST.get('delete_data', False)
     if isinstance(delete_data, str):
@@ -305,6 +315,7 @@ class SettingsView(View):
         return HttpResponse(json.dumps(self.load_settings()), content_type='application/json')
 
     def post(self, request):
+        if settings.READ_ONLY: return HttpResponse('It is in read-only mode.')
         settings = self.load_settings()
         data = json.loads(request.body.decode('utf-8'))
         settings.update(data["settings"])
