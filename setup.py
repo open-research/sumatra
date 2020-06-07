670c525d5555c34ca3d4001ef572722862347d8f
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2020-06-07 18:19:52 +0200

    Limit to Django v 1.x

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class sdist_git(sdist):
         return repo.head.commit.hexsha[:7]
 
 
-install_requires = ['Django>=1.8', 'django-tagging', 'httplib2',
+install_requires = ['Django>=1.8, <2', 'django-tagging', 'httplib2',
                     'docutils', 'jinja2', 'parameters', 'future']
 major_python_version, minor_python_version, _, _, _ = sys.version_info
 if major_python_version < 3 or (major_python_version == 3 and minor_python_version < 4):
